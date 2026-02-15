c5a4fa7e7d3e056d73417b9ee1cebec205c19cc2
x0y14 <ya01ma08@gmail.com>
2026-02-15 14:59:20 +0900

    移動

--- packages/markdoc/src/index.ts
+++ packages/markdoc/src/index.ts
@@ -3,8 +3,10 @@ import { parse, type Node } from "@markdoc/markdoc"
 // re-export
 export { parse, Node }
 //
-export { Frontmatter, filterFrontmatter } from "./frontmatter"
-export { buildConfig } from "./config"
-export { filterTagAttributes } from "./tag"
-export { rejectFunctions } from "./function"
+export { filterFrontmatter, type Frontmatter } from "./transform"
+export { buildConfig } from "./transform"
+export { filterTagAttributes } from "./transform"
+export { rejectFunctions } from "./transform"
 export { transformMarkdoc, type Components, type Restrictions } from "./transform"
+export { matchesDenyPattern } from "./props"
+export { filterProps, type FilterPropsInput, type PropsSchema } from "./props"
---
+++ packages/markdoc/src/transform/index.ts
@@ -0,0 +1,5 @@
+export { filterFrontmatter, type Frontmatter } from "./frontmatter"
+export { rejectFunctions } from "./function"
+export { filterTagAttributes } from "./tag"
+export { buildConfig } from "./config"
+export { transformMarkdoc, type Components, type Restrictions } from "./transform"
